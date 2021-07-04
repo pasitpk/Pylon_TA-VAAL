3d40b0125bca53b6c91708c2c1afe75d4bf02af1
pasitpk <pasit.j1995@gmail.com>
2021-07-04 11:13:43 +0700

    train pipeline

--- losses.py
+++ losses.py
@@ -4,23 +4,28 @@ from torch import nn
 
 class TaskLoss(nn.Module):
     
-    def __init__(self):
+    def __init__(self, lamb=1):
         super(TaskLoss, self).__init__()
 
+        self.lamb = lamb
         self.bce_loss = nn.BCEWithLogitsLoss()
 
     def forward(self, output, target):
-        return self.bce_loss(output, target)
+        return self.lamb * self.bce_loss(output, target)
 
 
 class RankLoss(nn.Module):
     
-    def __init__(self):
+    def __init__(self, lamb=1):
         super(RankLoss, self).__init__()
 
-        self.bce_loss = nn.BCEWithLogitsLoss()
+        self.lamb = lamb
+        self.bce_loss = nn.BCEWithLogitsLoss(reduction=None)
+        self.rank_loss = nn.BCEWithLogitsLoss()
 
-    def forward(self, pred_losses, target_losses):
+    def forward(self, pred_losses, output, target):
+
+        target_losses = self.bce_loss(output, target).detach()
 
         pred_losses_i, pred_losses_j = torch.split(pred_losses, 2)
         target_losses_i, target_losses_j = torch.split(target_losses, 2)
@@ -28,29 +33,31 @@ class RankLoss(nn.Module):
         pred_diff = pred_losses_i - pred_losses_j
         target_labels = (target_losses_i > target_losses_j).float()
 
-        return self.bce_loss(pred_diff, target_labels)
+        return self.lamb * self.rank_loss(pred_diff, target_labels)
 
 
 class VAELoss(nn.Module):
 
-    def __init__(self, beta=1):        
+    def __init__(self, lamb=1, beta=1):        
          super(VAELoss, self).__init__()
 
+         self.lamb = lamb
          self.beta = beta
          self.mse_loss = nn.MSELoss()
 
     def forward(self, x, recon, mu, logvar):
         mse = self.mse_loss(recon, x)
         kld = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
-        return mse + kld * self.beta
+        return self.lamb * (mse + kld * self.beta)
 
 
 class DiscLoss(nn.Module):
     
-    def __init__(self):
+    def __init__(self, lamb=1):
         super(DiscLoss, self).__init__()
 
+        self.lamb = lamb
         self.bce_loss = nn.BCEWithLogitsLoss()
 
     def forward(self, output, target):
-        return self.bce_loss(output, target)
+        return self.lamb * self.bce_loss(output, target)
--- trainer.py
+++ trainer.py
@@ -4,17 +4,18 @@ import torch
 from torch.cuda.amp import autocast
 from tqdm.auto import tqdm
 from scipy.special import expit
+from itertools import cycle
 
 
 def batch_acc(y_preds, y_targets):
     y_true = y_targets.cpu().detach().numpy()
     y_pred = (expit(y_preds.cpu().detach().numpy()) > 0.5).astype(int)
-    
+
     return (y_true == y_pred).mean(axis=0)
-    
+
 
 def run_train(
-    save_dir, 
+    save_dir,
     task_model,
     vae,
     disc,
@@ -36,8 +37,11 @@ def run_train(
     val_patient=None,
     device='cuda',
     grad_scaler=None,
-    ):
-    
+):
+
+    unlabeled_train_iter = cycle(unlabeled_train_loader)
+    unlabeled_val_iter = cycle(unlabeled_val_loader)
+
     train_loss_rec = []
     train_task_loss_rec = []
     train_vae_loss_rec = []
@@ -45,7 +49,7 @@ def run_train(
     train_acc_rec = []
     cur_train_patient = train_patient
     train_best_loss = np.inf
-    
+
     val_loss_rec = []
     val_task_loss_rec = []
     val_vae_loss_rec = []
@@ -53,13 +57,13 @@ def run_train(
     val_acc_rec = []
     cur_val_patient = val_patient
     val_best_loss = np.inf
-    
+
     for epoch in range(1, epochs + 1):
-        
+
         if cur_train_patient == 0 or cur_val_patient == 0:
             print('===== Early stop was reached =====')
             break
-            
+
         new_best = False
 
         batch_train_loss_rec = []
@@ -68,12 +72,37 @@ def run_train(
         batch_train_disc_loss_rec = []
         batch_train_acc_rec = []
 
-        for batch_train_X, batch_train_y in tqdm(train_loader):
-            batch_train_loss, batch_train_acc = train(model, batch_train_X, batch_train_y, criterion_train, optimizer, device, grad_scaler, batch_acc)
+        for batch_labeled_train_X, batch_labeled_train_y in tqdm(labeled_train_loader):
+            batch_unlabeled_train_X = next(unlabeled_train_iter)
+            batch_train_task_loss, batch_train_vae_loss, batch_train_disc_loss, batch_train_acc = train(
+                task_model,
+                vae,
+                disc,
+                batch_labeled_train_X,
+                batch_labeled_train_y,
+                batch_unlabeled_train_X,
+                task_loss,
+                rank_loss,
+                vae_loss,
+                disc_loss,
+                task_model_optimizer,
+                vae_optimizer,
+                disc_optimizer,
+                device,
+                grad_scaler,
+                batch_acc
+            )
+            batch_train_loss = sum([batch_train_task_loss, batch_train_vae_loss, batch_train_disc_loss])
             batch_train_loss_rec.append(batch_train_loss)
+            batch_train_task_loss_rec.append(batch_train_task_loss)
+            batch_train_vae_loss_rec.append(batch_train_vae_loss)
+            batch_train_disc_loss_rec.append(batch_train_disc_loss)
             batch_train_acc_rec.append(batch_train_acc)
 
         train_loss_rec.append(np.mean(batch_train_loss_rec))
+        train_task_loss_rec.append(np.mean(batch_train_task_loss_rec))
+        train_vae_loss_rec.append(np.mean(batch_train_vae_loss_rec))
+        train_disc_loss_rec.append(np.mean(batch_train_disc_loss_rec))
         train_acc_rec.append(np.mean(np.stack(batch_train_acc_rec), axis=0))
 
         batch_val_loss_rec = []
@@ -82,14 +111,33 @@ def run_train(
         batch_val_disc_loss_rec = []
         batch_val_acc_rec = []
 
-        for batch_val_X, batch_val_y in val_loader:
+        for batch_labeled_val_X, batch_labeled_val_y in labeled_val_loader:
+            batch_unlabeled_val_X = next(unlabeled_val_iter)
+            batch_val_task_loss, batch_val_vae_loss, batch_val_disc_loss, batch_val_acc = evaluate(
+                task_model,
+                vae,
+                disc,
+                batch_labeled_val_X,
+                batch_labeled_val_y,
+                batch_unlabeled_val_X,
+                task_loss,
+                rank_loss,
+                vae_loss,
+                disc_loss,
+                device,
+                batch_acc)
 
-            batch_val_loss, batch_val_acc = evaluate(model, batch_val_X, batch_val_y, criterion_val, device, batch_acc)
-
+            batch_val_loss = sum([batch_val_task_loss, batch_val_vae_loss, batch_val_disc_loss])
             batch_val_loss_rec.append(batch_val_loss)
+            batch_val_task_loss_rec.append(batch_val_task_loss)
+            batch_val_vae_loss_rec.append(batch_val_vae_loss)
+            batch_val_disc_loss_rec.append(batch_val_disc_loss)
             batch_val_acc_rec.append(batch_val_acc)
 
         val_loss_rec.append(np.mean(batch_val_loss_rec))
+        val_task_loss_rec.append(np.mean(batch_val_task_loss_rec))
+        val_vae_loss_rec.append(np.mean(batch_val_vae_loss_rec))
+        val_disc_loss_rec.append(np.mean(batch_val_disc_loss_rec))
         val_acc_rec.append(np.mean(np.stack(batch_val_acc_rec), axis=0))
 
         if (lr_scheduler is not None) and (lr_scheduler_target is not None):
@@ -101,33 +149,33 @@ def run_train(
                 lr_schd_target = train_loss_rec[-1]
 
             lr_scheduler.step(lr_schd_target)
-        
+
         if train_loss_rec[-1] > train_best_loss:
             if train_patient is not None:
                 cur_train_patient -= 1
-            
+
         else:
             train_best_loss = train_loss_rec[-1]
             cur_train_patient = train_patient
-            
+
         if val_loss_rec[-1] > val_best_loss:
             if val_patient is not None:
                 cur_val_patient -= 1
-            
+
         elif val_loss_rec[-1] >= train_loss_rec[-1]:
             val_best_loss = val_loss_rec[-1]
             cur_val_patient = val_patient
-            
+
             # to extend training
             train_best_loss = train_loss_rec[-1]
             cur_train_patient = train_patient
-            
+
             prev_checkpoints = os.listdir(save_dir)
             for prev_checkpoint in prev_checkpoints:
                 prev_checkpoint_path = os.path.join(save_dir, prev_checkpoint)
                 if prev_checkpoint_path.endswith('.tar'):
                     os.remove(prev_checkpoint_path)
-                        
+
             new_best = True
 
         print('epoch {}: train_loss = {:.4f},    valid_loss = {:.4f}\
@@ -135,87 +183,191 @@ def run_train(
             \n         train_vae_loss = {:.4f},    valid_vae_loss = {:.4f}\
             \n         train_disc_loss = {:.4f},    valid_disc_loss = {:.4f}\
             \n         train_slice_acc = {:.4f},    valid_slice_acc = {:.4f}'.format(
-                                                                              epoch,
-                                                                              train_loss_rec[-1],
-                                                                              val_loss_rec[-1],
-                                                                              train_task_loss_rec[-1],
-                                                                              val_task_loss_rec[-1],
-                                                                              train_vae_loss_rec[-1],
-                                                                              val_vae_loss_rec[-1],
-                                                                              train_disc_loss_rec[-1],
-                                                                              val_disc_loss_rec[-1],
-                                                                              train_acc_rec[-1][0],
-                                                                              val_acc_rec[-1][0],
-                                                                              ))
-        
+            epoch,
+            train_loss_rec[-1],
+            val_loss_rec[-1],
+            train_task_loss_rec[-1],
+            val_task_loss_rec[-1],
+            train_vae_loss_rec[-1],
+            val_vae_loss_rec[-1],
+            train_disc_loss_rec[-1],
+            val_disc_loss_rec[-1],
+            train_acc_rec[-1][0],
+            val_acc_rec[-1][0],
+        ))
+
         if new_best or epoch == epochs or cur_train_patient == 0 or cur_val_patient == 0:
             torch.save({
-                        'epoch': epoch,
-                        'cur_train_patient': cur_train_patient,
-                        'cur_val_patient': cur_val_patient,
-                        'task_model_state_dict': task_model.state_dict(),
-                        'vae_state_dict': vae.state_dict(),
-                        'disc_state_dict': disc.state_dict(),
-                        'task_model_optimizer_state_dict': task_model_optimizer.state_dict(),
-                        'vae_optimizer_state_dict': vae_optimizer.state_dict(),
-                        'disc_optimizer_state_dict': disc_optimizer.state_dict(),
-                        'lr_scheduler_state_dict': lr_scheduler.state_dict() if lr_scheduler is not None else None,
-                        'grad_scaler_state_dict': grad_scaler.state_dict() if grad_scaler is not None else None,
-                        'train_loss_rec': train_loss_rec,
-                        'val_loss_rec': val_loss_rec,
-                        'train_task_loss_rec': train_task_loss_rec,
-                        'val_task_loss_rec': val_task_loss_rec,
-                        'train_vae_loss_rec': train_vae_loss_rec,
-                        'val_vae_loss_rec': val_vae_loss_rec,
-                        'train_disc_loss_rec': train_disc_loss_rec,
-                        'val_disc_loss_rec': val_disc_loss_rec,
-                        'train_acc_rec': train_acc_rec,
-                        'val_acc_rec': val_acc_rec,
-                        }, os.path.join(save_dir, 'check_point_{}_loss_{:.4f}.tar'.format(epoch, val_loss_rec[-1])))
+                'epoch': epoch,
+                'cur_train_patient': cur_train_patient,
+                'cur_val_patient': cur_val_patient,
+                'task_model_state_dict': task_model.state_dict(),
+                'vae_state_dict': vae.state_dict(),
+                'disc_state_dict': disc.state_dict(),
+                'task_model_optimizer_state_dict': task_model_optimizer.state_dict(),
+                'vae_optimizer_state_dict': vae_optimizer.state_dict(),
+                'disc_optimizer_state_dict': disc_optimizer.state_dict(),
+                'lr_scheduler_state_dict': lr_scheduler.state_dict() if lr_scheduler is not None else None,
+                'grad_scaler_state_dict': grad_scaler.state_dict() if grad_scaler is not None else None,
+                'train_loss_rec': train_loss_rec,
+                'val_loss_rec': val_loss_rec,
+                'train_task_loss_rec': train_task_loss_rec,
+                'val_task_loss_rec': val_task_loss_rec,
+                'train_vae_loss_rec': train_vae_loss_rec,
+                'val_vae_loss_rec': val_vae_loss_rec,
+                'train_disc_loss_rec': train_disc_loss_rec,
+                'val_disc_loss_rec': val_disc_loss_rec,
+                'train_acc_rec': train_acc_rec,
+                'val_acc_rec': val_acc_rec,
+            }, os.path.join(save_dir, 'check_point_{}_loss_{:.4f}.tar'.format(epoch, val_loss_rec[-1])))
 
 
-def train(model, X, y, criterion, optimizer, device='cuda', grad_scaler=None, metric=None):
+def train(
+    task_model,
+    vae,
+    disc,
+    labeled_X,
+    labeled_y,
+    unlabeled_X,
+    task_criterion,
+    rank_criterion,
+    vae_criterion,
+    disc_criterion,
+    task_model_optimizer,
+    vae_optimizer,
+    disc_optimizer,
+    device='cuda',
+    grad_scaler=None,
+    metric=None,
+    ):
 
-    model.train()
-    X, y = X.to(device), y.to(device)
-    optimizer.zero_grad()
+    task_model.train()
+    vae.train()
+    disc.train()
+
+    labeled_X, labeled_y, unlabeled_X = labeled_X.to(device), labeled_y.to(device), unlabeled_X.to(device)
+    labeled_factor = len(labeled_X) / (len(labeled_X) + len(unlabeled_X))
+    unlabeled_factor = 1 - labeled_factor
+
+    task_model_optimizer.zero_grad()
+    vae_optimizer.zero_grad()
+    disc_optimizer.zero_grad()
+
+    # labeled data
+    with autocast(enabled=grad_scaler is not None):
+        task_output_l = task_model(labeled_X)
+        loss_rank_l = torch.sigmoid(task_output_l['loss_pred'])
+        recon_l, z_r_l, mu_l, logvar_l = vae(labeled_X, loss_rank_l)
+        disc_output_l = disc(z_r_l)
+        
+        task_loss = task_criterion(task_output_l['pred'], labeled_y)
+        rank_loss = rank_criterion(task_output_l['loss_pred'], task_output_l['pred'], labeled_y)
+        labeled_vae_loss = vae_criterion(labeled_X, recon_l, mu_l, logvar_l) * labeled_factor
+        labeled_disc_loss = disc_criterion(disc_output_l, torch.ones_like(disc_output_l)) * labeled_factor
+
+    if grad_scaler is not None:
+        grad_scaler.scale(task_loss).backward(retain_graph=True)
+        grad_scaler.scale(rank_loss).backward(retain_graph=True)
+        grad_scaler.scale(labeled_vae_loss).backward(retain_graph=True)
+        grad_scaler.scale(labeled_disc_loss).backward()
+    
+    else:
+        task_loss.backward(retain_graph=True)
+        rank_loss.backward(retain_graph=True)
+        labeled_vae_loss.backward(retain_graph=True)
+        labeled_disc_loss.backward()
+
+
+    # unlabeled data
+    with autocast(enabled=grad_scaler is not None):
+        task_output_u = task_model(unlabeled_X)
+        loss_rank_u = torch.sigmoid(task_output_u['loss_pred'])
+        recon_u, z_r_u, mu_u, logvar_u = vae(unlabeled_X, loss_rank_u)
+        disc_output_u = disc(z_r_u)
+        
+        unlabeled_vae_loss = vae_criterion(unlabeled_X, recon_u, mu_u, logvar_u) * unlabeled_factor
+        unlabeled_disc_loss = disc_criterion(disc_output_u, torch.zeroes_like(disc_output_u)) * unlabeled_factor
 
     if grad_scaler is not None:
 
-        with autocast():
-            output = model(X)['pred']
-            loss = criterion(output, y)
+        grad_scaler.scale(unlabeled_vae_loss).backward(retain_graph=True)
+        grad_scaler.scale(unlabeled_disc_loss).backward()
 
-        grad_scaler.scale(loss).backward()
-        grad_scaler.step(optimizer)
+        grad_scaler.step(task_model_optimizer)
+        grad_scaler.step(vae_optimizer)
+        grad_scaler.step(disc_optimizer)
+
         grad_scaler.update()
 
     else:
-        output = model(X)['pred']
-        loss = criterion(output, y)
+        unlabeled_vae_loss.backward(retain_graph=True)
+        unlabeled_disc_loss.backward()
 
-        loss.backward()
-        optimizer.step()
-        
+        task_model_optimizer.step()
+        vae_optimizer.step()
+        disc_optimizer.step()
+
+
+    total_task_loss = task_loss + rank_loss
+    vae_loss = labeled_vae_loss + unlabeled_vae_loss
+    disc_loss = labeled_disc_loss + unlabeled_disc_loss
+
     if metric is not None:
-        return loss.item(), metric(output, y)
+        return total_task_loss.item(), vae_loss.item(), disc_loss.item(), metric(task_output_l['pred'], labeled_y)
 
-    return loss.item()
+    return total_task_loss.item(), vae_loss.item(), disc_loss.item()
 
 
 @torch.no_grad()
-def evaluate(model, X, y, criterion, device='cuda', metric=None):
+def evaluate(
+    task_model,
+    vae,
+    disc,
+    labeled_X,
+    labeled_y,
+    unlabeled_X,
+    task_criterion,
+    rank_criterion,
+    vae_criterion,
+    disc_criterion,
+    device='cuda',
+    metric=None,
+    ):
 
-    model.eval()
+    task_model.eval()
+    vae.eval()
+    disc.eval()
 
-    X, y = X.to(device), y.to(device)
-    output = model(X)['pred']
+    labeled_X, labeled_y, unlabeled_X = labeled_X.to(device), labeled_y.to(device), unlabeled_X.to(device)
+    labeled_factor = len(labeled_X) / (len(labeled_X) + len(unlabeled_X))
+    unlabeled_factor = 1 - labeled_factor
 
-    loss = criterion(output, y).item()
-
-    model.train()
+    # labeled data
+    task_output_l = task_model(labeled_X)
+    loss_rank_l = torch.sigmoid(task_output_l['loss_pred'])
+    recon_l, z_r_l, mu_l, logvar_l = vae(labeled_X, loss_rank_l)
+    disc_output_l = disc(z_r_l)
     
-    if metric is not None:
-        return loss, metric(output, y)
+    task_loss = task_criterion(task_output_l['pred'], labeled_y)
+    rank_loss = rank_criterion(task_output_l['loss_pred'], task_output_l['pred'], labeled_y)
+    labeled_vae_loss = vae_criterion(labeled_X, recon_l, mu_l, logvar_l) * labeled_factor
+    labeled_disc_loss = disc_criterion(disc_output_l, torch.ones_like(disc_output_l)) * labeled_factor
 
-    return loss
+    # unlabeled data
+    task_output_u = task_model(unlabeled_X)
+    loss_rank_u = torch.sigmoid(task_output_u['loss_pred'])
+    recon_u, z_r_u, mu_u, logvar_u = vae(unlabeled_X, loss_rank_u)
+    disc_output_u = disc(z_r_u)
+    
+    unlabeled_vae_loss = vae_criterion(unlabeled_X, recon_u, mu_u, logvar_u) * unlabeled_factor
+    unlabeled_disc_loss = disc_criterion(disc_output_u, torch.zeroes_like(disc_output_u)) * unlabeled_factor
+    
+    total_task_loss = task_loss + rank_loss
+    vae_loss = labeled_vae_loss + unlabeled_vae_loss
+    disc_loss = labeled_disc_loss + unlabeled_disc_loss
+
+    if metric is not None:
+        return total_task_loss.item(), vae_loss.item(), disc_loss.item(), metric(task_output_l['pred'], labeled_y)
+
+    return total_task_loss.item(), vae_loss.item(), disc_loss.item()
+
